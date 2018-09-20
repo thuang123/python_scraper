07e08722d460dba064e55ce6742b7346a4864da1
Tony Huang <thuang_@hotmail.com>
2018-09-19 18:40:29 -0700

    Add child page scraping

--- scraper.py
+++ scraper.py
@@ -5,22 +5,39 @@ import igraph
 import re
 import Queue
 
-def makeGraph(initialPage, visitLimit):
+# Max number of child pages to scrape for each visited page
+PAGE_VISIT_LIMIT = 1
+
+def makeGraph(initialPage, graph):
     page = urllib2.urlopen(initialPage)
     soup = BeautifulSoup(page, "html.parser")
-    links = getPageLinks(soup, visitLimit)
+    links = getPageLinks(soup)
 
-    graph = igraph.Graph(directed=True)
     pageTitle = getPageTitleFromLink(initialPage)
     graph.add_vertex(pageTitle)
 
+    childPagesToVisit = []
+
     for link in links:
         childPageTitle = getPageTitleFromLink(link)
-        graph.add_vertex(childPageTitle)
+        if not vertexExists(graph, childPageTitle):
+            childPagesToVisit.append("https://en.wikipedia.org/wiki/" + childPageTitle)
+            print initialPage + childPageTitle
+            graph.add_vertex(childPageTitle)
         graph.add_edges([(pageTitle, childPageTitle)])
         graph.add_edges([(childPageTitle, pageTitle)])
 
+    for childPage in childPagesToVisit:
+        graph = makeGraph(childPage, graph)
+
     return graph
+
+def vertexExists(graph, vertex):
+    try:
+        graph.vs.find(name=vertex)
+    except ValueError:
+        return False
+    return True
 
 def getPageTitleFromLink(link):
     title = link.split("/wiki/")[1]
@@ -29,7 +46,7 @@ def getPageTitleFromLink(link):
     else:
         raise Exception("Unable to numberOfLinksToRetrieve page title.  Invalid wiki page encountered.")
 
-def getPageLinks(soupObject, numberOfLinksToRetrieve):
+def getPageLinks(soupObject):
     allDivs = soupObject.find_all("div")
     allBodyContent = []
     for div in allDivs:
@@ -39,18 +56,18 @@ def getPageLinks(soupObject, numberOfLinksToRetrieve):
                 allBodyContent.append(div);
     allLinks = []
     for bodyContent in allBodyContent:
-        if len(allLinks) < numberOfLinksToRetrieve:
-            allLinks = allLinks + getLinksHelper(bodyContent, numberOfLinksToRetrieve)
+        if len(allLinks) < PAGE_VISIT_LIMIT:
+            allLinks = allLinks + getLinksHelper(bodyContent, PAGE_VISIT_LIMIT)
         else:
             break
     return allLinks
 
-def getLinksHelper(bodyContent, numberOfLinksToRetrieve):
+def getLinksHelper(bodyContent, PAGE_VISIT_LIMIT):
     anchors = bodyContent.find_all("a")
     links = []
     validWikiPagePattern = "\/wiki\/[A-Z|a-z|_]*[A-Z|a-z]$"
     for anchor in anchors:
-        if len(links) < numberOfLinksToRetrieve:
+        if len(links) < PAGE_VISIT_LIMIT:
             link = anchor.get("href")
             if link is not None and re.match(validWikiPagePattern, link):
                 links.append(link)
@@ -59,7 +76,7 @@ def getLinksHelper(bodyContent, numberOfLinksToRetrieve):
     return links
 
 def main():
-    graph = makeGraph("https://en.wikipedia.org/wiki/Cat", 5)
+    graph = makeGraph("https://en.wikipedia.org/wiki/Cat", igraph.Graph(directed=True))
     print graph
 
 if __name__ == '__main__':
